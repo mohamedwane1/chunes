dc7ac0644f1c2099eef3583d7757622e0449d84e
mohamedwane1 <wane@wisc.edu>
2019-08-21 13:19:24 -0500

    Updates

---
+++ precache-manifest.a08211972b2123bf7ce429e26b244f34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98ad074c4aa39dfb98b7f9761f40d57",
+    "url": "/chunes/index.html"
+  },
+  {
+    "revision": "8cb3e2653a70a9829623",
+    "url": "/chunes/static/css/main.3cd73c45.chunk.css"
+  },
+  {
+    "revision": "f0f754bb018a42e714da",
+    "url": "/chunes/static/js/2.d0a21dec.chunk.js"
+  },
+  {
+    "revision": "8cb3e2653a70a9829623",
+    "url": "/chunes/static/js/main.300aceaf.chunk.js"
+  },
+  {
+    "revision": "e0fe936a9671438573a9",
+    "url": "/chunes/static/js/runtime~main.f424cdf2.js"
+  },
+  {
+    "revision": "6f0f76e56c1d38fb20d5f2680b086e43",
+    "url": "/chunes/static/media/video.6f0f76e5.mp4"
+  }
+]);
